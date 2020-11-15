8e3a5c0f1d881a766a22c9034e55e3213b185226
Yusaku Kotakemori <yusaku@kotasakuMBP.local>
2020-11-15 16:24:29 +0900

    Finish rails_task6

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+require "csv"
+require "faker"
+Faker::Config.locale = :ja
+
+# 1000件の初期データを投入
+1000.times do
+  User.create!(name: Faker::Name.name, age: rand(0..100))
+end
+
+puts "初期データの投入に成功しました！"
